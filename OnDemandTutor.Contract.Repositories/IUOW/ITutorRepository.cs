737b5fc601f45a46b6ac9c690a69b7256809df1b
KienQuoc <kieukienquocvn@gmail.com>
2024-09-17 20:05:40 +0700

    API Tutor

---
+++ OnDemandTutor.Contract.Repositories/IUOW/ITutorRepository.cs
@@ -0,0 +1,16 @@
+﻿using OnDemandTutor.Contract.Repositories.Entity;
+using OnDemandTutor.Contract.Repositories.Interface;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace OnDemandTutor.Contract.Repositories.IUOW
+{
+    public interface ITutorRepository : IGenericRepository<TutorSubject>
+    {
+        //Task<IEnumerable<TutorSubject>> GetAllAsync();
+        //Task<TutorSubject> GetByIdAsync(Guid id);
+        //Task AddAsync(TutorSubject tutorSubject);
+        //Task UpdateAsync(TutorSubject tutorSubject);
+        //Task DeleteAsync(Guid id);
+    }
+}
---
+++ OnDemandTutor.ModelViews/TutorSubjectModelViews/CreateTutorSubjectModelViews.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnDemandTutor.ModelViews.TutorSubjectModelViews
+{
+    public class CreateTutorSubjectModelViews
+    {
+        public Guid TutorId { get; set; }
+        public Guid SubjectId { get; set; }
+    }
+}
