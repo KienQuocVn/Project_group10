9b7990d41a47ab9e15cef5455646d0d722a406f0
ThuanDeepTry <nguyenminhthuan12a8tp1@gmail.com>
2024-09-27 16:40:16 +0700

    Update IUnitOfWork.cs

--- OnDemandTutor.Contract.Repositories/IUOW/IUnitOfWork.cs
+++ OnDemandTutor.Contract.Repositories/IUOW/IUnitOfWork.cs
@@ -6,17 +6,12 @@ namespace OnDemandTutor.Contract.Repositories.Interface
 {
     public interface IUnitOfWork : IDisposable
     {
-        IGenericRepository<T> GetRepository<T>() where T : class; // Phương thức lấy repository chung cho mọi loại thực thể
-        IGenericRepository<Schedule> ScheduleRepository { get; } // Repository cho Schedule
-        IGenericRepository<TutorSubject> TutorRepository { get; } // Repository cho TutorSubject
-        IGenericRepository<Subject> SubjectRepository { get; } // Repository cho Subject
-        IGenericRepository<Feedback> FeedbackRepository { get; } // Repository cho Feedback
-        IGenericRepository<Complaint> ComplaintRepository { get; } // Đã sửa từ ComplaintRpository sang ComplaintRepository
-        IGenericRepository<T> GetRepository<T>() where T : class;
+        IGenericRepository<T> GetRepository<T>() where T : class; 
         IGenericRepository<Schedule> ScheduleRepository { get; }
-        IGenericRepository<TutorSubject> TutorRepository { get; }
-        IGenericRepository<Subject> SubjectRepository { get; }
-        IGenericRepository<Feedback> FeedbackRepository { get; }
+        IGenericRepository<TutorSubject> TutorRepository { get; } 
+        IGenericRepository<Subject> SubjectRepository { get; } 
+        IGenericRepository<Feedback> FeedbackRepository { get; } 
+        IGenericRepository<Complaint> ComplaintRepository { get; } 
         IGenericRepository<Class> ClassRepository { get; }
 
 
@@ -25,11 +20,5 @@ namespace OnDemandTutor.Contract.Repositories.Interface
         void BeginTransaction();
         void CommitTransaction();
         void RollBack();
-
-        void Save(); // Lưu thay đổi
-        Task SaveAsync(); // Lưu thay đổi không đồng bộ
-        void BeginTransaction(); // Bắt đầu giao dịch
-        void CommitTransaction(); // Cam kết giao dịch
-        void RollBack(); // Hoàn tác giao dịch
     }
 }
