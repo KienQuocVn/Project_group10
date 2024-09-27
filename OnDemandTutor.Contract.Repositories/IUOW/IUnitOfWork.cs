d717821f358f5e0c2bd807e9072dee0233045b0e
ThuanDeepTry <nguyenminhthuan12a8tp1@gmail.com>
2024-09-27 16:20:48 +0700

    update complaint

--- OnDemandTutor.Contract.Repositories/IUOW/IUnitOfWork.cs
+++ OnDemandTutor.Contract.Repositories/IUOW/IUnitOfWork.cs
@@ -1,21 +1,22 @@
 ﻿using OnDemandTutor.Contract.Repositories.Entity;
 using OnDemandTutor.Contract.Repositories.IUOW;
+using System;
 
 namespace OnDemandTutor.Contract.Repositories.Interface
 {
     public interface IUnitOfWork : IDisposable
     {
-        IGenericRepository<T> GetRepository<T>() where T : class;
-        IGenericRepository<Schedule> ScheduleRepository { get; }
-        IGenericRepository<TutorSubject> TutorRepository { get; }
-        IGenericRepository<Subject> SubjectRepository { get; }
-        IGenericRepository<Feedback> FeedbackRepository { get; }
+        IGenericRepository<T> GetRepository<T>() where T : class; // Phương thức lấy repository chung cho mọi loại thực thể
+        IGenericRepository<Schedule> ScheduleRepository { get; } // Repository cho Schedule
+        IGenericRepository<TutorSubject> TutorRepository { get; } // Repository cho TutorSubject
+        IGenericRepository<Subject> SubjectRepository { get; } // Repository cho Subject
+        IGenericRepository<Feedback> FeedbackRepository { get; } // Repository cho Feedback
+        IGenericRepository<Complaint> ComplaintRepository { get; } // Đã sửa từ ComplaintRpository sang ComplaintRepository
 
-        void Save();
-        Task SaveAsync();
-        void BeginTransaction();
-        void CommitTransaction();
-        void RollBack();
-
+        void Save(); // Lưu thay đổi
+        Task SaveAsync(); // Lưu thay đổi không đồng bộ
+        void BeginTransaction(); // Bắt đầu giao dịch
+        void CommitTransaction(); // Cam kết giao dịch
+        void RollBack(); // Hoàn tác giao dịch
     }
 }
---
+++ OnDemandTutor.ModelViews/ComplaintModelViews/ResponseComplaintModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnDemandTutor.ModelViews.ComplaintModelViews
+{
+    public class ResponseComplaintModel
+    {
+        public Guid Id { get; set; }
+        public Guid StudentId { get; set; }
+        public Guid TutorId { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string Status { get; set; }
+    }
+}
