3e0718da9917361e79ee6868db54d0df01abfb41
KienQuoc <114797268+KienQuocVn@users.noreply.github.com>
2024-09-30 13:09:52 +0700

    Update IUnitOfWork.cs

--- OnDemandTutor.Contract.Repositories/IUOW/IUnitOfWork.cs
+++ OnDemandTutor.Contract.Repositories/IUOW/IUnitOfWork.cs
@@ -8,19 +8,15 @@ namespace OnDemandTutor.Contract.Repositories.Interface
     {
         IGenericRepository<T> GetRepository<T>() where T : class; 
         IGenericRepository<Schedule> ScheduleRepository { get; }
-<<<<<<< HEAD
         IGenericRepository<TutorSubject> TutorRepository { get; }
         IGenericRepository<Subject> SubjectRepository { get; }
         IGenericRepository<Feedback> FeedbackRepository { get; }
         IGenericRepository<Slot> SlotRepository { get; }
-=======
         IGenericRepository<TutorSubject> TutorRepository { get; } 
         IGenericRepository<Subject> SubjectRepository { get; } 
         IGenericRepository<Feedback> FeedbackRepository { get; } 
         IGenericRepository<Complaint> ComplaintRepository { get; } 
         IGenericRepository<Class> ClassRepository { get; }
-
->>>>>>> 736f99d09baea832d78df3e5777752264735af48
 
         void Save();
         Task SaveAsync();
