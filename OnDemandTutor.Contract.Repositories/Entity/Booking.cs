58703eec76eedb16054ac575c2694d0b880cbac0
DIEP TRAN <152488904+DiepTranCFPT@users.noreply.github.com>
2024-10-13 20:00:39 +0700

    Update Booking.cs

--- OnDemandTutor.Contract.Repositories/Entity/Booking.cs
+++ OnDemandTutor.Contract.Repositories/Entity/Booking.cs
@@ -7,20 +7,11 @@ namespace OnDemandTutor.Contract.Repositories.Entity
 {
     public class Booking : BaseEntity
     {
-<<<<<<< HEAD
+
         public Guid StudentId { get; set; }
         public Guid TutorId { get; set; }
         public Guid SubjectId { get; set; }
         public Guid SlotId { get; set; }
-=======
-        //public string Id { get; set; } = Guid.NewGuid().ToString(); // thay đổi
-        public Guid StudentId { get; set; }
-        public virtual Accounts Student { get; set; }
-
-        public string SubjectId { get; set; }
-        public virtual Subject Subject { get; set; }
-
->>>>>>> 991e0be3e63b3e2c7914793e6fa4efd988597bf0
         public DateTime BookingDate { get; set; }
         public double TotalPrice { get; set; }
 
