75f086621cf9419bcf77a6036ddb60099f563733
KienQuoc <114797268+KienQuocVn@users.noreply.github.com>
2024-10-18 14:04:46 +0700

    Update Slot.cs

--- OnDemandTutor.Contract.Repositories/Entity/Slot.cs
+++ OnDemandTutor.Contract.Repositories/Entity/Slot.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
@@ -11,22 +11,6 @@ namespace OnDemandTutor.Contract.Repositories.Entity
 {
     public class Slot : BaseEntity
     {
-        //[Key]
-        //public int Id { get; set; }
-
-        //[ForeignKey("Class")]
-        //public int ClassId { get; set; }
-
-        //public string DayOfSlot { get; set; }
-        //public TimeSpan StartTime { get; set; }
-        //public TimeSpan EndTime { get; set; }
-        //public int Price { get; set; }
-
-        //// Navigation properties
-        //public virtual Class Class { get; set; }
-        //public int Id { get; set; }
-
-
         public string ClassId { get; set; }
         public string DayOfSlot { get; set; }
         public TimeSpan StartTime { get; set; }
@@ -39,6 +23,5 @@ namespace OnDemandTutor.Contract.Repositories.Entity
         public virtual ICollection<Feedback> Feedbacks { get; set; }
         public virtual ICollection<Complaint> Complaints { get; set; }
         public virtual ICollection<Booking> Bookings { get; set; }
-
     }
 }
