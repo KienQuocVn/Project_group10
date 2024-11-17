98616f00195ba30351e00b889ef527836428eb06
Hyzmyn <hung2k3olaf@gmail.com>
2024-11-17 14:05:21 +0700

    razorpage

---
+++ OnDemandTutor.API/Pages/Payment/ProcessMonthEndSalary.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OnDemandTutor.API.Pages.Payment
+{
+    public class ProcessMonthEndSalaryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ OnDemandTutor.API/Pages/Payment/SubmitOrder.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OnDemandTutor.API.Pages.Payment
+{
+    public class SubmitOrderModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
